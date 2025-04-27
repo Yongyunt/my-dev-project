d330198c5275cc11119d5b0bb25940c33280420f
Yongyoot <sanT_T1998@hotmail.co.th>
2025-04-27 16:56:50 +0700

    edit admin CashSaleItem,QuotationItem

--- sales_app/admin.py
+++ sales_app/admin.py
@@ -13,15 +13,26 @@ class QuotationItemInline(admin.TabularInline):
 
 @admin.register(Quotation)
 class QuotationAdmin(admin.ModelAdmin):
-    list_display = ('quotation_date','id', 'customer', 'total_amount' ,'status')
+    list_display = ('id','quotation_date', 'customer', 'total_amount' ,'status')
     search_fields = ('customer__name',)
     list_filter = ('status',)
     inlines = [QuotationItemInline]
 
-@admin.register(QuotationItem)
 class QuotationItemAdmin(admin.ModelAdmin):
-    list_display = ('quotation', 'product', 'quantity', 'unit_price', 'subtotal')
-    search_fields = ('quotation__id', 'product__name')
+    list_display = ('product', 'quantity', 'unit_price')
+    
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'product':
+            kwargs['queryset'] = Product.objects.all()
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def save_model(self, request, obj, form, change):
+        # เมื่อบันทึกสินค้าใน quotation item ให้ดึงราคาจาก product
+        if obj.product:
+            obj.unit_price = obj.product.unit_price
+        super().save_model(request, obj, form, change)
+
+admin.site.register(QuotationItem, QuotationItemAdmin)
 
 class InvoiceItemInline(admin.TabularInline):
     model = InvoiceItem
@@ -60,7 +71,22 @@ class ReceiptAdmin(admin.ModelAdmin):
     list_display = ('receipt_date','id', 'customer__name','total_amount', 'payment_method')
     list_filter = ('payment_method',)
 
-admin.site.register(CashSale)
-admin.site.register(CashSaleItem)
+@admin.register(CashSale)
+class CashSaleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cash_sale_date', 'total_amount', 'payment_method')
+    list_filter = ('payment_method',)
+
+class CashSaleItemAdmin(admin.ModelAdmin):
+    list_display = ('cash_sale', 'product', 'quantity', 'unit_price', 'subtotal')
+    search_fields = ('cash_sale__id', 'product__name')
+    list_filter = ('cash_sale', 'product')
+    readonly_fields = ('subtotal',)
+    
+    def save_model(self, request, obj, form, change):
+        # Automatically calculate the subtotal
+        obj.subtotal = obj.quantity * obj.unit_price
+        super().save_model(request, obj, form, change)
+
+admin.site.register(CashSaleItem, CashSaleItemAdmin)
 
 
